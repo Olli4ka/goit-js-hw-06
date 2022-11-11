cdaad2815ae500b2322b232993a223f026bd1328
Olga Panayot <panayot.olga@gmail.com>
2022-11-11 19:46:43 +0200

    do task 4

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+const counter = document.querySelector("#value");
+let counterValue = 0;
+counter.innerHTML = `${counterValue}`;
+console.log(counterValue);
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+console.log(decrementBtn);
+decrementBtn.addEventListener("click", () => {
+	counterValue -= 1;
+	counter.innerHTML = `${counterValue}`;
+	console.log(counterValue);
+});
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+console.log(incrementBtn);
+incrementBtn.addEventListener("click", () => {
+	counterValue += 1;
+	counter.innerHTML = `${counterValue}`;
+	console.log(counterValue);
+});
