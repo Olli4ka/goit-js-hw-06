528f9219a5e19fd2d39ad6d89564a715d28ad013
Olga Panayot <panayot.olga@gmail.com>
2022-11-14 10:39:41 +0200

    do 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onLogin);
+
+function onLogin(event) {
+	event.preventDefault();
+
+	const formElements = event.currentTarget.elements;
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+	const formData = { email, password };
+
+	if (!email || !password) {
+		alert("all fields must be filled");
+	} else {
+		console.log(formData);
+		form.reset();
+	}
+}
