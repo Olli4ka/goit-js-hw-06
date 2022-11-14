410e7c86fa0b1d89cfccecfc01aa7c9507382acb
Olga Panayot <panayot.olga@gmail.com>
2022-11-14 12:55:33 +0200

    do 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+	body: document.querySelector("body"),
+	nameColor: document.querySelector(".color"),
+	changeBtn: document.querySelector(".change-color"),
+};
+
+refs.changeBtn.addEventListener("click", () => {
+	const color = getRandomHexColor();
+	refs.nameColor.textContent = color;
+	refs.body.style.backgroundColor = color;
+});
