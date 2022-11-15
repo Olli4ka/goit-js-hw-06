b55b6df76c2d3cac69b192cf9a82a59990366141
Olga Panayot <panayot.olga@gmail.com>
2022-11-15 11:55:25 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,13 @@
 const counter = document.querySelector("#value");
 let counterValue = 0;
-counter.innerHTML = `${counterValue}`;
+counter.textContent = `${counterValue}`;
 console.log(counterValue);
 
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 console.log(decrementBtn);
 decrementBtn.addEventListener("click", () => {
 	counterValue -= 1;
-	counter.innerHTML = `${counterValue}`;
+	counter.textContent = `${counterValue}`;
 	console.log(counterValue);
 });
 
@@ -15,6 +15,6 @@ const incrementBtn = document.querySelector('button[data-action="increment"]');
 console.log(incrementBtn);
 incrementBtn.addEventListener("click", () => {
 	counterValue += 1;
-	counter.innerHTML = `${counterValue}`;
+	counter.textContent = `${counterValue}`;
 	console.log(counterValue);
 });
